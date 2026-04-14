52896600b32a1e896fd1019804fed3c182ff4932
noobuy <nobilitypig@gmail.com>
2026-04-14 14:50:43 +0900

    v2

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-1_15nDMvr2K', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7qjniknkfqefdlmrmvr0m7cu0e', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'eu-west-1' // e.g. us-east-2
+        userPoolId: 'ap-northeast-2_IZMjgoiu7', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6ft9c9cnpp6ufdpe3ihb3pdqou', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://miq87fm8ef.execute-api.eu-west-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
